53e10c78c519621956754852c815042bafec75a1
Alberto Larios Espina <ale0014@alu.medac.es>
2026-04-07 11:48:48 +0200

    Add padding to body and alert on 'Inicio' link click

--- js/index.js
+++ js/index.js
@@ -1,2 +1,22 @@
-import feed from './feed.js';
 
+function emptyMain() {
+    const main = document.getElementById('contenido-principal');
+    main.innerHTML = '';
+}
+
+// Funciones clicks para cambiar el contenido del main
+elementInicio = document.getElementById('inicio');
+elementCatalogo = document.getElementById('catalogo');
+elementBuscador = document.getElementById('buscador');
+elementResenas = document.getElementById('resenas');
+elementEstadisticas = document.getElementById('estadisticas');
+elementRegister = document.getElementById('register');
+elementLogin = document.getElementById('login');
+
+elementInicio.addEventListener('click', mostrarInicio);
+elementCatalogo.addEventListener('click', mostrarCatalogo);
+elementBuscador.addEventListener('click', mostrarBuscador);
+elementResenas.addEventListener('click', mostrarResenas);
+elementEstadisticas.addEventListener('click', mostrarEstadisticas);
+elementRegister.addEventListener('click', mostrarRegister);
+elementLogin.addEventListener('click', mostrarLogin);
