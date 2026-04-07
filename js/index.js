0e17855bcf5a78c7592ee470cac689357afa27e8
Alberto Larios Espina <ale0014@alu.medac.es>
2026-04-07 14:21:34 +0200

    Refactor navigation links and update main content rendering functions

--- js/index.js
+++ js/index.js
@@ -1,22 +1,43 @@
+const main = document.body;
 
 function emptyMain() {
-    const main = document.getElementById('contenido-principal');
     main.innerHTML = '';
 }
 
-// Funciones clicks para cambiar el contenido del main
-elementInicio = document.getElementById('inicio');
-elementCatalogo = document.getElementById('catalogo');
-elementBuscador = document.getElementById('buscador');
-elementResenas = document.getElementById('resenas');
-elementEstadisticas = document.getElementById('estadisticas');
-elementRegister = document.getElementById('register');
-elementLogin = document.getElementById('login');
+function mostrarInicio() {
+    emptyMain();
+    main.innerHTML = `
+        <section id="contenido-principal">
+            dfgdfgsdfgdfgdfg
+        </section>
+        <aside id="sidebar-rss">
+            <h3>Últimas Novedades (RSS)</h3>
+            <div id="contenedor-feed"></div>
+        </aside>`;
+    
+}
 
-elementInicio.addEventListener('click', mostrarInicio);
+function mostrarCatalogo() {
+    emptyMain();
+    main.innerHTML = `
+        <section id="contenido-principal">
+            Catálogo de Libros
+        </section>`;
+}
+
+function alerta() {
+    alert("Funcionalidad en desarrollo");
+}
+
+// Funciones clicks para cambiar el contenido del main
+const elementInicio = document.getElementById('inicio');
+const elementCatalogo = document.getElementById('catalogo');
+const elementBuscador = document.getElementById('buscador');
+const elementResenas = document.getElementById('resenas');
+const elementEstadisticas = document.getElementById('estadisticas');
+const elementRegister = document.getElementById('register');
+const elementLogin = document.getElementById('login');
+
+elementInicio.addEventListener('click', alerta);
 elementCatalogo.addEventListener('click', mostrarCatalogo);
-elementBuscador.addEventListener('click', mostrarBuscador);
-elementResenas.addEventListener('click', mostrarResenas);
-elementEstadisticas.addEventListener('click', mostrarEstadisticas);
-elementRegister.addEventListener('click', mostrarRegister);
-elementLogin.addEventListener('click', mostrarLogin);
+
